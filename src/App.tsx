f68967b9699ebda2c9996e47258a7bfd59f2b597
Dzmitry Tsitarau <mikstat90@gmail.com>
2023-04-27 11:20:31 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import {Todolist} from "./Todolist"
+
+const tasks1 = [
+    { id: 1, title: "HTML&CSS", isDone: true },
+    { id: 2, title: "JS", isDone: true },
+    { id: 3, title: "ReactJS", isDone: false }
+]
+const tasks2 = [
+    { id: 1, title: "Hello world", isDone: true },
+    { id: 2, title: "I am Happy", isDone: false },
+    { id: 3, title: "Yo", isDone: false }
+]
+
+
+
+function App() {
+    return (
+        <div className="App">
+            <Todolist track1={'shapka 1'} track2={100200} tasks={tasks1} />
+            <Todolist track1={'shapka 2'} tasks={tasks2}/>
+        </div>
+    );
+}
+
+export default App;
